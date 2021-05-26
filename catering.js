26978fd08fe722bd9ed5943fb0b31e78476de813
Cakecreaxion <84874917+Cakecreaxion@users.noreply.github.com>
2021-05-26 15:49:09 -0400

    Add files via upload

---
+++ catering.js
@@ -0,0 +1,79 @@
+let numOfTimesPlayed = 0 ;
+
+const CateringVid = document.querySelectorAll(".cateringVid");
+const imgForVid = document.querySelectorAll(".vidImg");
+const playButton = document.getElementById("playicon3");
+const playButton1 = document.getElementById("playicon4");
+const playButton2 = document.getElementById("playicon5");
+
+
+
+imgForVid[0].addEventListener("click",()=>{ 
+    numOfTimesPlayed++;
+    RemoveClass(imgForVid[0], CateringVid[0])
+    playVid(CateringVid[0],playButton);
+
+});
+
+CateringVid[0].addEventListener("click",()=>{ 
+    numOfTimesPlayed++;
+    playVid(CateringVid[0],playButton);
+    
+
+});
+
+imgForVid[1].addEventListener("click",()=>{ 
+    numOfTimesPlayed++;
+    RemoveClass(imgForVid[1], CateringVid[1])
+    playVid(CateringVid[1],playButton1);
+
+});
+
+CateringVid[1].addEventListener("click",()=>{ 
+    numOfTimesPlayed++;
+    playVid(CateringVid[1],playButton1);
+    
+
+});
+
+imgForVid[2].addEventListener("click",()=>{ 
+    numOfTimesPlayed++;
+    RemoveClass(imgForVid[2], CateringVid[2])
+    playVid(CateringVid[2],playButton2);
+
+});
+
+CateringVid[2].addEventListener("click",()=>{ 
+    numOfTimesPlayed++;
+    playVid(CateringVid[2],playButton2);
+
+});
+
+
+function RemoveClass(imgToHidde,Vid){
+    imgToHidde.classList.add("none");
+    Vid.classList.remove("none")
+    console.log(imgToHidde.classList)
+
+}
+
+
+function playVid(vidToPlay,idOfpauseButton){
+    vidToPlay.play(); 
+    idOfpauseButton.classList.add("none");
+    idOfpauseButton.classList.remove("playiconstyle");
+
+    if(numOfTimesPlayed >= 3){
+        numOfTimesPlayed= 1;
+    }
+
+    if(numOfTimesPlayed >= 2){
+        idOfpauseButton.classList.remove("none");
+        idOfpauseButton.classList.add("playiconstyle");
+        vidToPlay.pause();
+        numOfTimesPlayed=0;
+        
+    }
+    
+    
+}
---
+++ index.js
@@ -0,0 +1,93 @@
+const pauseButton = document.getElementById("playicon");
+
+const playButton1 = document.querySelectorAll(".vid1");
+
+const cateringVid = document.querySelectorAll(".cateringVid");
+
+const pauseButton2 = document.getElementById("playicon1");
+
+const pauseButton3 = document.getElementById("playicon2");
+
+const img4Vid = document.querySelectorAll(".cakeVidImg");
+
+
+
+// const pauseButton4 = document.getElementById("playicon3");
+
+
+let numOfTimesPlayed= 0;
+
+
+
+img4Vid[0].addEventListener("click",()=>{
+    numOfTimesPlayed++;
+    removeClass(img4Vid[0],playButton1[0]);
+    playVid(playButton1[0],pauseButton3);
+})
+
+
+playButton1[0].addEventListener("click",()=>{
+    numOfTimesPlayed += 1;
+    playVid(playButton1[0],pauseButton3);
+});
+
+img4Vid[1].addEventListener("click",()=>{
+    numOfTimesPlayed++;
+    removeClass(img4Vid[1],playButton1[1]);
+    playVid(playButton1[1],pauseButton);
+})
+
+
+playButton1[1].addEventListener("click",()=>{
+    numOfTimesPlayed += 1;
+    playVid(playButton1[1],pauseButton);
+    
+});
+
+img4Vid[2].addEventListener("click",()=>{
+    numOfTimesPlayed++;
+    removeClass(img4Vid[2],playButton1[2]);
+    playVid(playButton1[2],pauseButton2);
+})
+
+playButton1[2].addEventListener("click",()=>{
+    numOfTimesPlayed += 1;
+    playVid(playButton1[2],pauseButton2);
+    
+});
+
+// cateringVid.addEventListener("click",()=>{
+//     numOfTimesPlayed += 1;
+//     playVid(cateringVid1[0],pauseButton4);
+    
+// });
+
+
+function removeClass (img,vid){
+    img.classList.add("none");
+    vid.classList.remove("none");
+}
+
+
+
+//add id of pause button conncented to videos
+function playVid(vidToPlay,idOfpauseButton){
+    vidToPlay.play(); 
+    idOfpauseButton.classList.add("none");
+    idOfpauseButton.classList.remove("playiconstyle");
+
+    if(numOfTimesPlayed >= 3){
+        numOfTimesPlayed= 1;
+    }
+
+    if(numOfTimesPlayed >= 2){
+        vidToPlay.pause();
+        numOfTimesPlayed=0;
+        
+    }
+    
+    
+}
+
+
+
